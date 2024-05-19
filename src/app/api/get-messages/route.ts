2b4dfb68bddbab73286dc4da2a90712c9f73633d
dharam-in <dharamdotin@gmail.com>
2024-05-19 13:17:16 +0530

    Mongodb Aggregation Pipeline Added

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -19,7 +19,24 @@ export async function GET(request: Request){
 
     const userId = new mongoose.Types.ObjectId(user._id);
     try {
-        
+        const user = await UserModel.aggregate([
+            {$match: {id: userId}},
+            {$unwind: '$messages'},
+            {$sort: {'messages.createdAt': -1}},
+            {$group: {_id: '$_id', messages: {$push: '$messages'}}}
+        ])
+
+        if(!user || user.length === 0){
+            return Response.json({
+                success: false,
+                messgae: "User Not Found"
+            }, {status: 500})
+        }
+
+        return Response.json({
+            success: true,
+            messages: user[0].messages
+        }, {status: 200})
     } catch (error) {
         
     }
