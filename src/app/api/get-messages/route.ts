9eb500366a1c351a2940e219fc1ad9475ce9aba8
dharam-in <dharamdotin@gmail.com>
2024-05-19 13:06:55 +0530

    Get Message Route and Accept Messages Route Created and now start mongodb Aggregation Pipeline

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,27 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User, getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import mongoose from "mongoose";
+
+export async function GET(request: Request){
+    await dbConnect();
+
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if(!session || !session.user){
+        return Response.json({
+            success: true,
+            message: "User not found"
+        }, {status: 401})
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
